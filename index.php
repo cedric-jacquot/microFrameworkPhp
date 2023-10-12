4c9840c646cb75b99eca71f74697ec13748205aa
cedric-jacquot <cedric.jacquot54@gmail.com>
2023-10-12 21:59:32 +0200

    fix

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ $route = new Routes;
 // return Controller and Method defined in Routes.php array
 $routeDatas = $route->findController();
 if ($routeDatas) {
-
     dump($routeDatas);
 
     // Create Class from Route
@@ -33,7 +32,7 @@ if ($routeDatas) {
 } else {
     // else return error
     $data = null;
-    http_response_code() === 500;
+    http_response_code(500);
 }
 
 // and html output
